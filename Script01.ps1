7ddbde9722e54d430d28b7c807cf645f0041a775
Luis Felipe Francisco <110519969+LuisFelipeFrancisco@users.noreply.github.com>
2022-11-08 22:28:48 -0300

    Create Script01.ps1

---
+++ Script01.ps1
@@ -0,0 +1,9 @@
+#Este é meu primeiro script do PowerShell
+Clear-Host
+Write-Output "olá mundo"
+$variavel = "valor qualquer que ficara armazenado na variavel"
+Write-Output $variavel
+$numero = 2.3
+$idade = 18
+Write-Output "numero: $numero"
+Write-Output "idade: $idade"
